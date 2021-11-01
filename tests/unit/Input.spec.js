0db786c5e5a131e0bd83c8cbd0f86ee66d58c12f
WIBR45919 <waffeu12092001@gmail.com>
2021-11-01 17:10:50 +0100

    test: Add Vue test utils in my project

--- tests/unit/Input.spec.js
+++ tests/unit/Input.spec.js
@@ -1,8 +1,16 @@
 import {mount} from "@vue/test-utils";
 import InputTask from "@/components/InputTask.vue";
 
+<<<<<<< Updated upstream
 describe('Input Task component', ()=>{
     test('', ()=>{
         const wrapper = mount(InputTask)
+=======
+describe('',()=>{
+    test('',()=>{
+        const wrapper = mount(InputTask)
+        const t = 1
+        expect(t).toBe(1)
+>>>>>>> Stashed changes
     })
 })
