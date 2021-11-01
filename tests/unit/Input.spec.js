a54e08590f0fb827141e0cd4080450a6bb548994
WIBR45919 <waffeu12092001@gmail.com>
2021-11-01 16:31:00 +0100

    test: Add vue test utils in this project

---
+++ tests/unit/Input.spec.js
@@ -0,0 +1,8 @@
+import {mount} from "@vue/test-utils";
+import InputTask from "@/components/InputTask.vue";
+
+describe('Input Task component', ()=>{
+    test('', ()=>{
+        const wrapper = mount(InputTask)
+    })
+})
