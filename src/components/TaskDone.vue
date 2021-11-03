12e59b77aa513f0616ce458b3f3bdcc081dd03eb
WIBR45919 <waffeu12092001@gmail.com>
2021-11-03 11:17:05 +0100

    test: Adjust test of TaskDone component

--- src/components/TaskDone.vue
+++ src/components/TaskDone.vue
@@ -5,18 +5,18 @@
               <input type="checkbox" :checked="isDone" @change="taskDo">
           </div>
           <div class="description-task">
-              <p> {{ description }}  <span class="date">{{ date }}</span></p>
+              <p> {{ description }} <span class="date">{{ date }}</span></p>
           </div>
           <div class="actions">
-          <span title="Time">
-              <svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px" fill="orange"><g><rect fill="none" height="24" width="24"/></g><g><g><g><path d="M12,2C6.5,2,2,6.5,2,12s4.5,10,10,10s10-4.5,10-10S17.5,2,12,2z M16.2,16.2L11,13V7h1.5v5.2l4.5,2.7L16.2,16.2z"/></g></g></g></svg>
-          </span>
-              <span title="Edit" @click="editTask">
-              <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="aqua"><path d="M0 0h24v24H0z" fill="none"/><path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"/></svg>
-          </span>
-              <span title="Delete" @click="deleteTask">
-              <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="darkred"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm5 11H7v-2h10v2z"/></svg>
-          </span>
+            <span title="Time" id="Time">
+                <svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px" fill="orange"><g><rect fill="none" height="24" width="24"/></g><g><g><g><path d="M12,2C6.5,2,2,6.5,2,12s4.5,10,10,10s10-4.5,10-10S17.5,2,12,2z M16.2,16.2L11,13V7h1.5v5.2l4.5,2.7L16.2,16.2z"/></g></g></g></svg>
+            </span>
+            <span title="Edit" @click="editTask" id="edit">
+                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="aqua"><path d="M0 0h24v24H0z" fill="none"/><path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"/></svg>
+            </span>
+            <span title="Delete" @click="deleteTask" id="delete">
+                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="darkred"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm5 11H7v-2h10v2z"/></svg>
+            </span>
           </div>
       </div>
   </div>
@@ -61,7 +61,7 @@
           }
 
             return{
-                taskDo,
+              taskDo,
               deleteTask,
               editTask,
             }
--- tests/unit/TaskDone.spec.js
+++ tests/unit/TaskDone.spec.js
@@ -3,12 +3,35 @@ import TaskDone from "@/components/TaskDone.vue";
 
 describe('Task Done component', ()=>{
     const wrapper = mount(TaskDone, {
-        props: { id: 0, description: '', date: '', isDone: false }
+        props: { id: 0, description: 'description', date: '12/09/2001', isDone: false }
     })
-    // it('should emit when task has done',  async () => {
-    //     const emitTaskDone = wrapper.find('input[type="checkbox"]').setChecked()
-    //     // wrapper.vm.$emit('check', 0, 'checked')
-    //     await emitTaskDone.trigger('change')
-    //     expect(wrapper.props('isDone')).toBe(true)
-    // });
+    const emitCheck = wrapper.find('input[type="checkbox"]')
+
+    it('should emit when task has done and if checkbox is checked',  async () => {
+        expect(emitCheck.exists()).toBe(true)
+        await emitCheck.trigger('change')
+        expect(wrapper.emitted('check')[0]).toEqual([wrapper.props('id'),'checked'])
+        expect(emitCheck)
+    });
+
+     it('should emit when task has deleted',  async () => {
+        const emitDeleted = wrapper.find('#delete')
+        expect(emitDeleted.exists()).toBe(true)
+        await emitDeleted.trigger('click')
+        expect(wrapper.emitted('delete')[0]).toEqual([wrapper.props('id')])
+    });
+
+    it('should emit when task want to be edit',  async () => {
+        const emitEdit = wrapper.find('#edit')
+        expect(emitEdit.exists()).toBe(true)
+        await emitEdit.trigger('click')
+        expect(wrapper.emitted('edit')[0]).toEqual([`${ wrapper.props('description') }`])
+    });
+
+    it('should have a corresponding description and date',  () => {
+        const divShow = wrapper.find('.description-task')
+        expect(divShow.exists()).toBe(true)
+        expect(divShow.find('p').html()).toBe(`<p>${ wrapper.props('description') } <span class="date">${ wrapper.props('date') }</span></p>`)
+    });
+
 })
